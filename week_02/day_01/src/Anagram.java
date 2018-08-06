07cfd897e272a00c5a8189cc6c4e1d0254ee51f2
Tamás Molnár <molnart90@gmail.com>
2018-08-06 17:00:18 +0200

    fixed Anagram and Reverse

--- week_02/day_01/src/Anagram.java
+++ week_02/day_01/src/Anagram.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Anagram {
+
+
   public static void main(String[] args) {
     System.out.println("Enter two strings and I'll let you know if they are anagrams:");
     Scanner reader = new Scanner (System.in);
@@ -14,10 +16,7 @@ public class Anagram {
     ArrayList<Character> secondArray=new ArrayList();
     stringToChar(stringTwo, secondArray);
     System.out.println(isAnagram(firstArray,secondArray));
-
-
   }
-
   public static ArrayList<Character> stringToChar(String input, ArrayList<Character> output) {
     for (int i=0; i<input.length(); i++){
       output.add(input.charAt(i));
@@ -27,10 +26,10 @@ public class Anagram {
   }
 
   public static boolean isAnagram(ArrayList<Character> arrayOne, ArrayList<Character> arrayTwo) {
-    boolean x=true;
+    boolean x=false;
     for (int i=0; i<arrayOne.size(); i++){
-    if (!arrayOne.contains(arrayTwo.get(i))) {
-      x=false;
+    if (arrayOne.size()==(arrayTwo.size())&&arrayOne.get(i)==arrayTwo.get(i)) {
+      x=true;
     }
     }
   return x;}
--- week_02/day_01/src/Reverse.java
+++ week_02/day_01/src/Reverse.java
@@ -7,7 +7,7 @@ public class Reverse {
 // - Reverse the order of the elements in `aj`
 // - Print the elements of the reversed `aj`
   public static void main(String[] args) {
-    int[] aj={3,4,5,6,7};
+    Integer[] aj={3,4,5,6,7};
     Collections.reverse(Arrays.asList(aj));
 
     for (int j = 0; j < aj.length; j++) {
