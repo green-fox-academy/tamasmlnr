5c637872e0301e8ccaf3cac22bfd6f0945c4a9f2
Tamás Molnár <molnart90@gmail.com>
2018-08-06 15:08:02 +0200

    Fixed palindromes - added functions

--- week_02/day_01/src/Palindrome.java
+++ week_02/day_01/src/Palindrome.java
@@ -5,25 +5,26 @@ import java.util.Scanner;
 public class Palindrome {
 
   public static void main(String[] args) {
-    Scanner reader = new Scanner (System.in);
+    Scanner reader = new Scanner(System.in);
     System.out.println("Enter the word: ");
     String word = reader.nextLine();
-
-    char[] newArray = new char[word.length()];
-    System.out.print(word);
-    for (int i=0; i<word.length(); i++)
-    {
-      newArray[word.length()-1-i]=word.charAt(i);
-    }
-    String result ="";
-    for (char x: newArray)
-    {
-      result+=x;
-    }
-
+    String result = createPalindrome(word);
     System.out.println(result);
 
-       }
-
   }
+
+  public static String createPalindrome(String word) {
+    char[] newArray = new char[word.length()];
+    System.out.print(word);
+    for (int i = 0; i < word.length(); i++) {
+      newArray[word.length() - 1 - i] = word.charAt(i);
+    }
+    String result = "";
+    for (char x : newArray) {
+      result += x;
+    }
+    return result;
+  }
+
+}
 
