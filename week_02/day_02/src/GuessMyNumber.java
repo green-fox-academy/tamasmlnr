291b4f9f561dab03a6be32926683c0a480457824
Tamás Molnár <molnart90@gmail.com>
2018-08-07 14:36:13 +0200

    Completed GuessMyNumber

--- week_02/day_02/src/GuessMyNumber.java
+++ week_02/day_02/src/GuessMyNumber.java
@@ -2,15 +2,39 @@ import java.util.Scanner;
 
 public class GuessMyNumber {
   public static void main(String[] args) {
-   int max=generateRandom();
-    System.out.println(max);
+    Scanner reader = new Scanner(System.in);
+    int randomNum = generateRandom(reader);
+    guesser(randomNum, reader);
   }
 
-  public static int generateRandom() {
+  public static int generateRandom(Scanner reader) {
     System.out.println("What is the highest number for the random?");
-    Scanner reader = new Scanner(System.in);
-    int max=Integer.parseInt(reader.nextLine());
+    int max = Integer.parseInt(reader.nextLine());
     int randomNumber = (int) (Math.random() * max);
+    System.out.println("I've the number between 1-" +max +". You have 5 lives.");
     return randomNumber;
+  }
+
+  public static void guesser(int numberToGuess, Scanner reader) {
+    int lives = 5;
+
+    while (true) {
+      int guess = Integer.parseInt(reader.nextLine());
+      if (guess < numberToGuess) {
+        lives -= 1;
+        System.out.println("Too low. You have " + lives + " lives left.");
+      }
+      if (guess > numberToGuess) {
+        lives -= 1;
+        System.out.println("Too high. You have " + lives + " lives left.");
+      }
+      if (guess == numberToGuess) {
+        System.out.println("Congratulations. You won!");
+        break;
+      }
+      if (lives == 0) {
+        System.out.println("You lost. But do not get disappointed, life goes on and there is so much beauty in it. \nLosing one game does not mean you are worthless. Learning how to lose sometimes feel better than winning. So embrace it!");
+      }
+    }
   }
 }
