7040bf988791673c21edc26475ba5bcfbc770bf5
Tamás Molnár <molnart90@gmail.com>
2018-08-14 14:10:21 +0200

    Add text files for day 2 exercises

--- week_02/day_02/src/random.java
+++
@@ -1,9 +0,0 @@
-import java.util.Random;
-
-public class random {
-  public static void main(String[] args) {
-    Random random = new Random();
-    System.out.println(random.nextInt((10) + 5));
-
-  }
-}
