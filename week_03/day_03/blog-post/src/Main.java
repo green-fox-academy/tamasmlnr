b025d340071b0acb013e9bf06ec45d4abe569aed
Tamás Molnár <molnart90@gmail.com>
2018-08-15 11:54:50 +0200

    BlogPost completed

---
+++ week_03/day_03/blog-post/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	BlogPost LoremIpsum= new BlogPost("Jon Doe","Lorem Ipsum", "Lorem ipsum dolor sit amet.", "2000.05");
+	BlogPost ButWhy= new BlogPost("Tim Urban","Wait But Why", "A popular long-form, stick-figure-illustrated blog about almost everything.", "2010.10.10");
+	BlogPost Trump= new BlogPost("William Turton","One Engineer Is Trying to Get IBM to Reckon With Trump", "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.", "2017.03.27");
+    }
+}
