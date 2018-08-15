00d54b75bc34a260d86e502bca58c5fb7df709ab
Tamás Molnár <molnart90@gmail.com>
2018-08-15 11:46:44 +0200

    First example completed

---
+++ week_03/day_03/Post-it/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+  public static void main(String[] args) {
+    // write your code here
+        PostIt myPostit= new PostIt("orange", "Idea 1", "blue");
+        PostIt myPostitTwo= new PostIt("pink", "Awesome", "black");
+        PostIt myPostitThree= new PostIt("yellow", "Superb", "green");
+  }
+}
