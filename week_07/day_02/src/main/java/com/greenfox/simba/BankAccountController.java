bc3487df84991b11d621a08fb35e1027b5b8851b
Tamás Molnár <molnart90@gmail.com>
2018-09-17 15:37:13 +0200

    Add text.html - th:utext & th:text

--- week_07/day_02/src/main/java/com/greenfox/simba/BankAccountController.java
+++ week_07/day_02/src/main/java/com/greenfox/simba/BankAccountController.java
@@ -12,10 +12,26 @@ public class BankAccountController {
 
   @RequestMapping("/list")
   public String showList(Model model) {
+    BankAccount account = new BankAccount("Simba", 2000, "Lion");
     model.addAttribute("name", account.getName());
     model.addAttribute("balance", account.getBalance());
     model.addAttribute("animal", account.getAnimalType());
-  return "account";
+    return "account";
   }
+
+  @RequestMapping("/text")
+  public String showText(Model model) {
+    model.addAttribute("text", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
+    return "text";
+  }
+
+  @RequestMapping("/list")
+  public String showMultiple(Model model) {
+    model.addAttribute("name", account.getName());
+    model.addAttribute("balance", account.getBalance());
+    model.addAttribute("animal", account.getAnimalType());
+    return "account";
+  }
+
 
 }
