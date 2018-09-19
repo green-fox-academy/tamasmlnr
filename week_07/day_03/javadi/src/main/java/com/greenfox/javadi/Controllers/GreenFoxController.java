bfc95eb6ae299ebe116baf1073d16980f252587b
Tamás Molnár <molnart90@gmail.com>
2018-09-19 14:28:39 +0200

    Add new student adder

--- week_07/day_03/javadi/src/main/java/com/greenfox/javadi/Controllers/GreenFoxController.java
+++ week_07/day_03/javadi/src/main/java/com/greenfox/javadi/Controllers/GreenFoxController.java
@@ -1,14 +1,44 @@
 package com.greenfox.javadi.Controllers;
 
+import com.greenfox.javadi.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class GreenFoxController {
 
+  @Autowired
+  StudentService studentService;
+
   @GetMapping("/gfa")
-  public String gfaMain() {
+  public String gfaMain(Model model) {
+    return "gfaindex";
+  }
+
+  @GetMapping("/gfa/list")
+  public String studentList(Model model) {
+    List<String> students = studentService.findAll();
+    model.addAttribute("students", students);
     return "studentlist";
+  }
+
+  @GetMapping("/gfa/add")
+  public String studentAdder(Model model) {
+    String studentName = "";
+    model.addAttribute("name", studentName);
+    return "addstudent";
+  }
+
+  @GetMapping("/gfa/save")
+  public String saveStudent(@RequestParam("name") String name, Model model) {
+    studentService.save(name);
+    return "redirect:list";
   }
 
 
