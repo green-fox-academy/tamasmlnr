19c1c8195d7b3941c5266f42ebac18b89af321aa
Tamás Molnár <molnart90@gmail.com>
2018-09-26 20:27:38 +0200

    Add post class

---
+++ week_08/day_05/redditproject/src/main/java/com/greenfox/reddit/Models/Post.java
@@ -0,0 +1,27 @@
+package com.greenfox.reddit.Models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Post {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+  private String content;
+  private String title;
+  private String url;
+
+  public Post(String content, String title, String url) {
+    this.content = content;
+    this.title = title;
+    this.url = url;
+  }
+}
