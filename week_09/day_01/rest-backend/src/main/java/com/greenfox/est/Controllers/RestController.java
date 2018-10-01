fc6d6d0907ab0b408f0ba2739168c25d114f6903
Tamás Molnár <molnart90@gmail.com>
2018-10-01 12:59:05 +0200

    Add AppendA endpoint

--- week_09/day_01/rest-backend/src/main/java/com/greenfox/est/Controllers/RestController.java
+++ week_09/day_01/rest-backend/src/main/java/com/greenfox/est/Controllers/RestController.java
@@ -1,12 +1,14 @@
 package com.greenfox.est.Controllers;
 
 
+import com.greenfox.est.Models.DTOs.Append;
 import com.greenfox.est.Models.DTOs.Doubling;
 import com.greenfox.est.Models.DTOs.ErrorClass;
 import com.greenfox.est.Models.DTOs.Greeting;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -31,6 +33,16 @@ public class RestController {
       return new ErrorClass("Please provide a title!");
     }
     return new Greeting(name, title);
+  }
+
+  @GetMapping("/appenda/{appendable}")
+  public ResponseEntity<?> doubling(@PathVariable(value = "appendable", required = false) String appendable) {
+    if (appendable != null) {
+      return ResponseEntity.status(HttpStatus.OK)
+          .body(new Append(appendable));
+    }
+    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        .build();
   }
 
 }
--- week_09/day_01/rest-backend/src/main/java/com/greenfox/est/Models/DTOs/Append.java
+++ week_09/day_01/rest-backend/src/main/java/com/greenfox/est/Models/DTOs/Append.java
@@ -1,4 +1,21 @@
 package com.greenfox.est.Models.DTOs;
 
 public class Append {
+
+  private String appended;
+
+  public Append(String toAppend) {
+    this.appended = toAppend + "a";
+  }
+
+  public Append() {
+  }
+
+  public String getAppended() {
+    return appended;
+  }
+
+  public void setAppended(String appended) {
+    this.appended = appended;
+  }
 }
