2d4d5e6d25bd028b287e1967cd45049836c32356
Tamás Molnár <molnart90@gmail.com>
2018-10-02 13:23:17 +0200

    Complete Groot endpoint

--- week_09/day_02/groot/src/main/java/com/greenfox/groot/Controller/GuardianController.java
+++ week_09/day_02/groot/src/main/java/com/greenfox/groot/Controller/GuardianController.java
@@ -1,18 +1,26 @@
 package com.greenfox.groot.Controller;
 
 
+import com.greenfox.groot.Models.ErrorMessage;
+import com.greenfox.groot.Models.GrootReply;
 import com.greenfox.groot.Models.Message;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GuardianController {
 
   @GetMapping("/groot")
-  public Object groot(@RequestBody(required = false) Message message) {
-
-    return null;
+  public ResponseEntity<?> groot(@RequestParam(value = "message", required = false) String message) {
+    if (message == null)
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body(new ErrorMessage("I am Groot!"));
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(new GrootReply(message, "I am groot!"));
   }
 
 
