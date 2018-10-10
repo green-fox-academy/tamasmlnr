062d78946b11019b328a40c2e22920182eb65203
Tamás Molnár <molnart90@gmail.com>
2018-10-10 10:41:03 +0200

    Add Post DTO

--- week_10/day03/redditproject/src/main/java/com/greenfox/reddit/Models/PostDTO.java
+++ week_10/day03/redditproject/src/main/java/com/greenfox/reddit/Models/PostDTO.java
@@ -1,4 +1,23 @@
 package com.greenfox.reddit.Models;
 
+import java.util.List;
+
 public class PostDTO {
+
+  private List<Post> posts;
+
+  public PostDTO(List<Post> posts) {
+    this.posts = posts;
+  }
+
+  public PostDTO() {
+  }
+
+  public List<Post> getPosts() {
+    return posts;
+  }
+
+  public void setPosts(List<Post> posts) {
+    this.posts = posts;
+  }
 }
