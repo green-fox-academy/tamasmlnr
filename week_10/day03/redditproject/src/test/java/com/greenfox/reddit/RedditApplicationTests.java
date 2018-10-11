41c759129d413b1291a93d97473fe1e28e7904c1
Tamás Molnár <molnart90@gmail.com>
2018-10-11 18:00:54 +0200

    Add first step in /api/allposts REST test

--- week_10/day03/redditproject/src/test/java/com/greenfox/reddit/RedditApplicationTests.java
+++ week_10/day03/redditproject/src/test/java/com/greenfox/reddit/RedditApplicationTests.java
@@ -11,14 +11,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -36,6 +42,8 @@ public class RedditApplicationTests {
   @MockBean
   private PostService postService;
 
+  private BCryptPasswordEncoder bCryptPasswordEncoder;
+
   private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
       MediaType.APPLICATION_JSON.getSubtype(),
       Charset.forName("utf8"));
@@ -47,19 +55,76 @@ public class RedditApplicationTests {
   }
 
 
+//  @Test
+//  @WithMockUser(username = "admin", password = "password")
+//  public void existentUserCanGetTokenAndAuthentication() throws Exception {
+////    String username = "admin";
+////    String password = "password";
+//
+////    String body = "{    \"username\": \"admin\",    \"password\": \"password\"}";
+//
+////    mockMvc.perform(post("login"))
+////        .andExpect(status().isOk())
+////        .andDo(print());
+//
+//
+//    mockMvc.perform(MockMvcRequestBuilders.post("/login")
+//        .contentType(contentType)
+//        .content(body)
+//        .accept(contentType))
+//        .andExpect(status().isOk());
+////
+////    String response = result.getResponse()
+////        .getContentAsString();
+////    response = response.replace("{\"access_token\": \"", "");
+////    String token = response.replace("\"}", "");
+////
+////    mockMvc.perform(MockMvcRequestBuilders.get("/test")
+////        .header("Authorization", "Bearer " + token))
+////        .andExpect(status().isOk());
+//  }
+
+
+//
   @Test
-  public void budgeReturnsRightAttractions() throws Exception {
-    List<Post> posts = new ArrayList<>();
+  @WithMockUser(username = "admin", password = "password")
+  public void existentUserCanGetTokenAndAuthentication() throws Exception {
+
+        List<Post> posts = new ArrayList<>();
     posts.add(new Post("Test", "Test"));
     PostDTO dto = new PostDTO(posts);
     when(postService.listAllPosts()).thenReturn(dto);
 
-    mockMvc.perform(get("/"))
+    mockMvc.perform(get("/api/allposts"))
         .andExpect(status().isOk())
         .andExpect(content().contentType(contentType))
-        .andExpect(jsonPath("$.attractions.size()", is(1)))
         .andDo(print());
+//
+//    String response = result.getResponse()
+//        .getContentAsString();
+//    response = response.replace("{\"access_token\": \"", "");
+//    String token = response.replace("\"}", "");
+//
+//    mockMvc.perform(MockMvcRequestBuilders.get("/test")
+//        .header("Authorization", "Bearer " + token))
+//        .andExpect(status().isOk());
   }
 
 
+//  @Test
+//  public void budgeReturnsRightAttractions() throws Exception {
+//    List<Post> posts = new ArrayList<>();
+//    posts.add(new Post("Test", "Test"));
+//    PostDTO dto = new PostDTO(posts);
+//    when(postService.listAllPosts()).thenReturn(dto);
+//
+//    mockMvc.perform(get("/"))
+//        .andExpect(status().isOk())
+//        .andExpect(content().contentType(contentType))
+//        .andExpect(jsonPath("$.attractions.size()", is(1)))
+//        .andDo(print());
+//  }
+
+
 }
+
