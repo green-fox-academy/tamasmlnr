be0feaa5b66b50dfb0eac87f65092406c2a1dfd5
Tamás Molnár <molnart90@gmail.com>
2018-10-12 10:21:53 +0200

    Clean up tests/ Add unauthroized POST test

--- week_10/day03/redditproject/src/test/java/com/greenfox/reddit/RedditApplicationTests.java
+++ week_10/day03/redditproject/src/test/java/com/greenfox/reddit/RedditApplicationTests.java
@@ -8,29 +8,31 @@ import com.greenfox.reddit.Service.PostService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.core.Is.is;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(PostAPIController.class)
@@ -49,48 +51,25 @@ public class RedditApplicationTests {
       Charset.forName("utf8"));
 
   @Test
-  public void getUnauthorizedErrorwithoutUser() throws Exception {
+  public void getUnauthorizedErrorWithoutUserOnAllPosts() throws Exception {
     mockMvc.perform(get("/api/allposts"))
         .andExpect(status().isUnauthorized());
   }
 
 
-//  @Test
-//  @WithMockUser(username = "admin", password = "password")
-//  public void existentUserCanGetTokenAndAuthentication() throws Exception {
-////    String username = "admin";
-////    String password = "password";
-//
-////    String body = "{    \"username\": \"admin\",    \"password\": \"password\"}";
-//
-////    mockMvc.perform(post("login"))
-////        .andExpect(status().isOk())
-////        .andDo(print());
-//
-//
-//    mockMvc.perform(MockMvcRequestBuilders.post("/login")
-//        .contentType(contentType)
-//        .content(body)
-//        .accept(contentType))
-//        .andExpect(status().isOk());
-////
-////    String response = result.getResponse()
-////        .getContentAsString();
-////    response = response.replace("{\"access_token\": \"", "");
-////    String token = response.replace("\"}", "");
-////
-////    mockMvc.perform(MockMvcRequestBuilders.get("/test")
-////        .header("Authorization", "Bearer " + token))
-////        .andExpect(status().isOk());
-//  }
+  @Test
+  public void getUnauthorizedErrorwithoutUserWhenPosting() throws Exception {
+    mockMvc.perform(post("/api/allposts"))
+        .andExpect(status().isForbidden());
+  }
 
 
-//
+
   @Test
   @WithMockUser(username = "admin", password = "password")
   public void existentUserCanGetTokenAndAuthentication() throws Exception {
 
-        List<Post> posts = new ArrayList<>();
+    List<Post> posts = new ArrayList<>();
     posts.add(new Post("Test", "Test"));
     PostDTO dto = new PostDTO(posts);
     when(postService.listAllPosts()).thenReturn(dto);
@@ -99,32 +78,12 @@ public class RedditApplicationTests {
         .andExpect(status().isOk())
         .andExpect(content().contentType(contentType))
         .andDo(print());
-//
-//    String response = result.getResponse()
-//        .getContentAsString();
-//    response = response.replace("{\"access_token\": \"", "");
-//    String token = response.replace("\"}", "");
-//
-//    mockMvc.perform(MockMvcRequestBuilders.get("/test")
-//        .header("Authorization", "Bearer " + token))
-//        .andExpect(status().isOk());
   }
 
 
-//  @Test
-//  public void budgeReturnsRightAttractions() throws Exception {
-//    List<Post> posts = new ArrayList<>();
-//    posts.add(new Post("Test", "Test"));
-//    PostDTO dto = new PostDTO(posts);
-//    when(postService.listAllPosts()).thenReturn(dto);
-//
-//    mockMvc.perform(get("/"))
-//        .andExpect(status().isOk())
-//        .andExpect(content().contentType(contentType))
-//        .andExpect(jsonPath("$.attractions.size()", is(1)))
-//        .andDo(print());
-//  }
+
 
 
 }
+
 
